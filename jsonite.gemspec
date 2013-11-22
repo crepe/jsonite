35f75b67735742734a93ed47f6866d30b8c10dbe
Evan Owen <kainosnoema@gmail.com>
2013-11-21 18:03:58 -0800

    Add fixtures and benchmark spec as baseline for performance fixes
    jasonite is actually a little slow right now. This sets up a simple
    test and a fairly high threshold for expected performance. It passes
    on a 2.8Ghz i7 MBP.
    
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- jsonite.gemspec
+++ jsonite.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'activesupport', '>= 3.1.0'
 
+  s.add_development_dependency 'activemodel', '>= 3.1.0'
   s.add_development_dependency 'rake', '= 10.1.0'
   s.add_development_dependency 'rspec', '= 2.14.1'
 end
---
+++ spec/benchmark_spec.rb
@@ -0,0 +1,38 @@
+require 'benchmark'
+require 'jsonite'
+require_relative 'support/fixtures'
+
+describe 'Jsonite performance' do
+
+  let(:documents) do
+    documents = 10.times.map do |i|
+      Document.new "Document #{i}", "/documents/#{i}", 'text/html', []
+    end
+
+    documents.size.times do |i|
+      documents[i].related_documents << documents[-i]
+    end
+
+    documents
+  end
+
+  # create a total of 175 nested objects (15 users, 160 documents)
+  let(:users) do
+    [
+      user1 = User.new('David', 26, [], documents.slice(0..5)),
+      user2 = User.new('Julie', 29, [ user1 ], documents.slice(3..8)),
+      user3 = User.new('Thomas', 28, [ user1, user2 ], documents.slice(6..9)),
+      User.new('Alfred', 24, [ user1, user2, user3 ], [])
+    ]
+  end
+
+  it 'presents hundreds of objects very quickly' do
+    time = Benchmark.realtime do |x|
+      # 175 * 10 times = 1750 total presentations
+      10.times { UserPresenter.present(users) }
+    end
+
+    time.should < 0.25
+  end
+
+end
