f561408e4702fa0b90c6958be6927d96ce259f57
David Celis <me@davidcel.is>
2015-02-15 16:24:58 -0800

    Fix plural roots with ActiveRecord >= 4.2.0
    In ActiveRecord 4.2.0, the `model_name` method was introduced to
    instances of ActiveRecord::Base classes. Now that this is defined on
    both individual records and collections, we need an additional check to
    see whether we're dealing with one record or a collection. Considering
    we use `respond_to? :to_ary` elsewhere, this patch duplicates that check
    in `Helper.resource_name`.
    
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/jsonite/helper.rb
+++ lib/jsonite/helper.rb
@@ -6,7 +6,7 @@ class Jsonite
     module_function
 
     def resource_name resource
-      if resource.respond_to? :model_name
+      if resource.respond_to?(:model_name) && resource.respond_to?(:to_ary)
         resource.model_name.collection
       elsif resource.class.respond_to? :model_name
         resource.class.model_name.singular
