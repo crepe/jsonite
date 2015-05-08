424e4ad215ffab09c1c462ae8cb6b3e1b26542b1
Evan Owen <kainosnoema@gmail.com>
2015-05-08 07:37:57 -0700

    Add __ suffix to ivars in LetsProxy for consistency with stdlib
    Signed-off-by: Evan Owen <kainosnoema@gmail.com>

--- lib/jsonite/lets_proxy.rb
+++ lib/jsonite/lets_proxy.rb
@@ -4,18 +4,18 @@ class Jsonite
     undef_method :==, :!, :!=
 
     def initialize object, context, lets = {}
-      @__object, @__context, @__lets = object, context, lets
+      @__object__, @__context__, @__lets__ = object, context, lets
 
-      @__memoized = ::Hash.new do |memoized, name|
-        memoized[name] = instance_exec @__context, &@__lets.fetch(name)
+      @__memoized__ = ::Hash.new do |memoized, name|
+        memoized[name] = instance_exec @__context__, &@__lets__.fetch(name)
       end
     end
 
     def method_missing name, *args, &block
-      if @__lets.key?(name.to_s)
-        @__memoized[name.to_s]
+      if @__lets__.key?(name.to_s)
+        @__memoized__[name.to_s]
       else
-        @__object.__send__ name, *args, &block
+        @__object__.__send__ name, *args, &block
       end
     end
 
